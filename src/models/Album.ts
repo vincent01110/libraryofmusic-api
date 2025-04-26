196d95a0bd2ccff9de1ce81933447b1cd6406ca5
vincent01110 <bencekristof06@gmail.com>
2025-04-26 16:22:12 +0200

    property left off

--- src/models/Album.ts
+++ src/models/Album.ts
@@ -1,6 +1,7 @@
 import { prop } from '@typegoose/typegoose';
 import { Image } from './Image';
 import mongoose from 'mongoose';
+import { Artist } from './Artist';
 
 export class Album {
     @prop()
@@ -12,12 +13,16 @@ export class Album {
     @prop({ required: true })
     public name: string;
 
+    @prop({ type: () => Artist, required: true })
+    public artists: Artist[];
+
     @prop({ type: () => Image, required: true })
     public images: Image[];
 
-    constructor(id: string, name: string, images: Image[]) {
+    constructor(id: string, name: string, artists: Artist[], images: Image[]) {
         this.id = id;
         this.name = name;
+        this.artists = artists;
         this.images = images;
     }
 }
